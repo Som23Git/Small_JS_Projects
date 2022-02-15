09f06da26ea30f58c772f9de19898c4f5a9f56e2
Som23Git <somesh.rokz@gmail.com>
2022-02-16 01:34:09 +0530

    Fibonacci Series

---
+++ Fibonacci_Series.js
@@ -0,0 +1,11 @@
+//Fibonacci Series
+
+console.log("It is Fibonacci Series"+fibonacci());
+
+function fibonacci(){
+    sum = 1;
+    for(i=0;i<10;i++){
+        sum = sum + i;
+        console.log(sum);
+    } return sum;
+}
