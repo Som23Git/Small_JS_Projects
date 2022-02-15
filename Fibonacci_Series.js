6e000eeb0b8efba4a1faa012e69dbbf624b7a127
Som23Git <somesh.rokz@gmail.com>
2022-02-16 02:43:37 +0530

    Fibonacci Series Updated Ver1.0

--- Fibonacci_Series.js
+++ Fibonacci_Series.js
@@ -1,11 +1,15 @@
 //Fibonacci Series
 
-console.log("It is Fibonacci Series"+fibonacci());
+console.log("It is Fibonacci Series "+fibonacci());
+
 
 function fibonacci(){
-    sum = 1;
-    for(i=0;i<10;i++){
-        sum = sum + i;
-        console.log(sum);
-    } return sum;
+    var n = 10;
+    var z = 0;
+    for (x=1,y=0,i=2;i<=n;i++){
+        console.log(z);
+        z=x+y;
+        x=y;
+        y=z;
+    } return z;
 }
