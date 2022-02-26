dbf893342d73a0eafb558d7969d773b34f7a971d
Som23Git <somesh.rokz@gmail.com>
2022-02-26 21:30:55 +0530

    Find the Christmas Date from Current Date using getTime() Method

--- christmasDate.js
+++ christmasDate.js
@@ -1,11 +1,17 @@
 //Write a JavaScript program to calculate days left until next Christmas.
 
 today=new Date();
-var cmas=new Date(today.getFullYear(), 11, 25);
+var chrisDate=new Date(today.getFullYear(), 11, 25);
 if (today.getMonth()==11 && today.getDate()>25) 
-{
-cmas.setFullYear(cmas.getFullYear()+1); 
+{ chrisDate.setFullYear = (chrisDate.getFullYear()+1); 
 }  
-var one_day=1000*60*60*24;
-console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+var one_day=1000*60*60*24; 
+console.log(one_day);
+console.log(chrisDate.getTime());
+console.log(today.getTime());
+console.log(Math.ceil((chrisDate.getTime()-today.getTime())/(one_day))+
 " days left until Christmas!");
+
+/* Output */
+
+/* 302 days left until Christmas! */
