01d90d4dd63c7427ede53b9bd0b668d05d0e51c1
Som23Git <93936168+Som23Git@users.noreply.github.com>
2022-02-28 23:58:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+{
+        num1 = document.getElementsByClassName("numberOne").value;
+        num2 = document.getElementsByClassName("numberTwo").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+    num1 = document.getElementsByClassName("numberOne").value;
+    num2 = document.getElementsByClassName("numberTwo").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
