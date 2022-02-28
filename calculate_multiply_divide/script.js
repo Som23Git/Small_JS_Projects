740cb91bda4772168e3451d7bbc192cab938c9e5
Som23Git <somesh.rokz@gmail.com>
2022-03-01 00:41:40 +0530

    Updates Script.js and Index.html

--- calculate_multiply_divide/script.js
+++ calculate_multiply_divide/script.js
@@ -1,14 +1,14 @@
 //This function multiplies the given numbers
 function multiplyBy()
 {
-        num1 = document.getElementsByClassName("numberOne").value;
-        num2 = document.getElementsByClassName("numberTwo").value;
+        num1 = document.getElementById("numberOne").value;
+        num2 = document.getElementById("numberTwo").value;
         document.getElementById("result").innerHTML = num1 * num2;
 }
 
 function divideBy() 
 { 
-    num1 = document.getElementsByClassName("numberOne").value;
-    num2 = document.getElementsByClassName("numberTwo").value;
-document.getElementById("result").innerHTML = num1 / num2;
+    num1 = document.getElementById("numberOne").value;
+    num2 = document.getElementById("numberTwo").value;
+    document.getElementById("result").innerHTML = num1 / num2;
 }
