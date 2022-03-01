41ad5d84f0621e556065a34e259261a38c9f50b9
Som23Git <somesh.rokz@gmail.com>
2022-03-01 22:09:14 +0530

    Multiply_Divide_With_Some_CSS and JS

--- calculate_multiply_divide/script.js
+++ calculate_multiply_divide/script.js
@@ -1,14 +1,28 @@
 //This function multiplies the given numbers
 function multiplyBy()
 {
-        num1 = document.getElementById("numberOne").value;
-        num2 = document.getElementById("numberTwo").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+    num1 = document.getElementById("numberOne").value;
+    num2 = document.getElementById("numberTwo").value;
+    valMutiply = num1 * num2;
+    console.log(valMutiply);
+    decisionNum = isNaN(valMutiply);
+    if(decisionNum == true) {
+    document.getElementById("result").innerHTML = "Please Enter a Valid Number Input";
+    } else {
+    document.getElementById("result").innerHTML = valMutiply; 
+    }
 }
 
 function divideBy() 
 { 
     num1 = document.getElementById("numberOne").value;
     num2 = document.getElementById("numberTwo").value;
-    document.getElementById("result").innerHTML = num1 / num2;
+    valDivide = num1 / num2;
+    console.log(valDivide);
+    decisionNum = isNaN(valDivide);
+    if(decisionNum == true) {
+        document.getElementById("result").innerHTML = "Please Enter a Valid Number Input";
+    } else {
+        document.getElementById("result").innerHTML = valDivide; 
+    }
 }
