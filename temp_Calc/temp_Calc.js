f4b6477f654c62fa3be7e1249154f41a43d9d6d7
Som23Git <somesh.rokz@gmail.com>
2022-03-02 23:49:57 +0530

    Commiting Temperature Calculator

---
+++ temp_Calc/temp_Calc.js
@@ -0,0 +1,7 @@
+/* Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit. 
+[ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ] */
+
+function tempCalc() {
+    num1 = document.getElementById("calcCelsius").value;
+    num2 = document.getElementById("calcFheit").value;
+}
