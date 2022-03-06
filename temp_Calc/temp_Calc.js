e2261128adf279b088b84bc5f65ea1ed0c6e5893
Som23Git <somesh.rokz@gmail.com>
2022-03-06 12:06:14 +0530

    Updated Changes w.r.t. Degree Symbols in result

--- temp_Calc/temp_Calc.js
+++ temp_Calc/temp_Calc.js
@@ -4,12 +4,12 @@
 function celSius() {
     var num1 = document.getElementById("calcCelsius").value;
     var resFheit = ((num1/5) * 9) + 32;
-    document.getElementById("result").innerHTML = resFheit + " Degree Fahrenheit";
+    document.getElementById("result").innerHTML = resFheit + ' <span>&#8457;</span>';
 }
 
 function fahrHeit() {
     var num2 = document.getElementById("calcFheit").value;
     var resCels = Math.round(((num2-32)/9) * 5);
-    document.getElementById("result").innerHTML = resCels + " Degree Celsius";
+    document.getElementById("result").innerHTML = resCels + ' <span>&#8451;</span>';
 }
     
