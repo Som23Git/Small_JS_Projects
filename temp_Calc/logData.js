037d7bf242ae1fd0a4548827ac1204523acda2d5
Som23Git <somesh.rokz@gmail.com>
2022-03-14 22:05:41 +0530

    Updated the event listeners for local storage in the logs page

--- temp_Calc/logData.js
+++ temp_Calc/logData.js
@@ -1,8 +1,14 @@
 window.addEventListener('storage', () => {
    const xlogs = localStorage.getItem('myObj');
    console.log(xlogs);
-   document.getElementById("database_1").innerHTML = xlogs;
+   document.getElementById("database_1").innerHTML = document.getElementById("database_1").innerHTML + xlogs + '</br>';
 }); 
+
+window.addEventListener('load', () => {
+    const xlogs = localStorage.getItem('myObj');
+    console.log(xlogs);
+    document.getElementById("database_1").innerHTML = document.getElementById("database_1").innerHTML + xlogs + '</br>';
+ }); 
 
 //function getLogs(){
 //    xlogs = localStorage.getItem('myObj');
