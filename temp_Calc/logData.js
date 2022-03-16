465a42e2464a4c3611c8154bd40fa92d433bdc9a
Som23Git <somesh.rokz@gmail.com>
2022-03-16 11:19:52 +0530

    Updated the storage of multi array data

--- temp_Calc/logData.js
+++ temp_Calc/logData.js
@@ -1,14 +1,16 @@
 window.addEventListener('storage', () => {
-   const xlogs = localStorage.getItem('myObj');
-   console.log(xlogs);
-   document.getElementById("database_1").innerHTML = document.getElementById("database_1").innerHTML + xlogs + '</br>';
+    var newDataItems = localStorage.getItem("myStoreData");
+    console.log("NewDataItems:" + newDataItems);
+    document.getElementById("database_1").innerHTML = newDataItems + '</br>';
 }); 
 
-window.addEventListener('load', () => {
-    const xlogs = localStorage.getItem('myObj');
-    console.log(xlogs);
-    document.getElementById("database_1").innerHTML = document.getElementById("database_1").innerHTML + xlogs + '</br>';
- }); 
+
+
+// window.addEventListener('load', () => {
+//     const xlogs = localStorage.getItem('myObj');
+//     console.log(xlogs);
+//     document.getElementById("database_1").innerHTML = document.getElementById("database_1").innerHTML + xlogs + '</br>';
+//  }); 
 
 //function getLogs(){
 //    xlogs = localStorage.getItem('myObj');
