a52e15d8e557b0c8b81b10a7a83f782eb0016c61
Som23Git <somesh.rokz@gmail.com>
2022-03-19 22:20:27 +0530

    Updated the basic components with data-attributes

---
+++ basic-calculator/script.js
@@ -0,0 +1,7 @@
+const numButtons = document.querySelectorAll('[data-number]');
+const operButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButtons = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
