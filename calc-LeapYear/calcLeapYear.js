c144d77e91aab8588ea9c39919a14d2dd2d9fbc1
Som23Git <somesh.rokz@gmail.com>
2022-03-23 23:37:40 +0530

    Updated the code to display the appropriate text

--- calc-LeapYear/calcLeapYear.js
+++ calc-LeapYear/calcLeapYear.js
@@ -1,20 +1,22 @@
 console.log("starting...");
 
-
 function calcLeap(){
-    var x = document.getElementById("inputYear");
+    var x = document.getElementById("inputYear").value;
     if(x % 4 == 0 && x % 100 == 0 && x % 400 == 0){
         document.getElementById("displayOp").innerHTML = "It's a century leap year";
         console.log("It's a century leap year");
     }
     else if (x % 4 == 0 && x % 100 == 0 && x % 400 != 0){
-        document.write("It's a century but not a leap year");
+        document.getElementById("displayOp").innerHTML = "It's a century but not a leap year";
+        console.log("It's a century but not a leap year");
     }
     else if(x % 4 == 0 && x % 100 != 0 && x % 400 != 0) {
-        document.write("It's a leap year");
+        document.getElementById("displayOp").innerHTML = "It's a leap year";
+        console.log("It's a leap year");
     }
     else {
-        document.write("It's NOT a leap year");
+        document.getElementById("displayOp").innerHTML = "It's NOT a leap year";
+        console.log("It's NOT a leap year");
     }
 }
 
