93e45add4d014a20368afa6d269764e3e26400f8
Som23Git <somesh.rokz@gmail.com>
2022-04-05 21:32:08 +0530

    Basic Problem Solving - 4 using split() method and .toLocaleLowerCase()

--- ballparkOrders.js
+++ ballparkOrders.js
@@ -23,3 +23,57 @@ Sample Output
 Explanation
 Because Popcorn is not on the menu, this friend gets a coke which brings the subtotal to $25.00 and $26.75 with tax.
 --------------------------------------------------------------------------------------------------------------------- */
+
+bpOrder = (x) => {
+let text = x;
+text = text.toLocaleLowerCase();
+// console.log(text); To check whether the words are converted to lowercase.
+const myArray = text.split(" ");
+console.log(myArray);
+let sum = 0;
+for(i=0; i<myArray.length; i++){
+switch(myArray[i]){
+    case "nachos":
+    case "pizza":
+        sum = sum + 6;
+        break;
+    case "cheeseburger":
+        sum = sum + 10;
+        break;
+    case "water":
+        sum = sum + 4;
+        break;
+    case "coke":
+        sum = sum + 5;
+        break;
+    default:
+        sum = sum + 5;
+        break;
+}
+console.log(sum);
+}
+sum = sum + (sum * 0.07);
+console.log(`The total is ${sum}`);
+}
+
+bpOrder("Pizza Cheeseburger Water Popcorn");
+bpOrder("nachos CheeseBurger POPCorn Coke");
+
+
+/* OUTPUT */
+/* 
+[ 'pizza', 'cheeseburger', 'water', 'popcorn' ]
+6
+16
+20
+25
+The total is 26.75
+
+[ 'nachos', 'cheeseburger', 'popcorn', 'coke' ]
+6
+16
+21
+26
+The total is 27.82
+
+*/
