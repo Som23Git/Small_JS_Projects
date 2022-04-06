60581b0c132a5284c072b04740d459b3874d77e7
Som23Git <somesh.rokz@gmail.com>
2022-04-06 21:11:13 +0530

    How to initiate an array using a constructor

--- data-structures/arrays.js
+++ data-structures/arrays.js
@@ -6,10 +6,9 @@ function DVDBOX(name, releaseYear, director){
     console.log(`Name of the movie is ${this.name} and releaseYear is ${this.releaseYear} and the director is ${this.director}`);
 };
 
-let dvdBox = new Array(2);
+let dvdBox = new Array(5);
 dvdBox[0] = new DVDBOX("ram","2018","Steven Spielberg");
 dvdBox[1] = new DVDBOX("Kir","2020","2567 Spielberg");
 dvdBox[2] = new DVDBOX("shah","2010","Sdirec Spielberg");
 dvdBox[3] = new DVDBOX("preshant","2018","Steven noel");
-
 dvdBox[4] = new DVDBOX("uma","2000","Steven director");
