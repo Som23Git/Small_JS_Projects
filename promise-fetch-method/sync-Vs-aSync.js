e64ebcc95cf184af4406728d5dfd1310c9cd5b0a
Som23Git <somesh.rokz@gmail.com>
2022-05-03 22:53:35 +0530

    The Basic Implementation and difference of Synchronous and Asynchronous JavaScript

--- promise-fetch-method/sync-Vs-aSync.js
+++ promise-fetch-method/sync-Vs-aSync.js
@@ -1,4 +1,4 @@
-//Synchronous and Asynchronous JavaScript
+//Synchronous JavaScript
 
 let log = console.log;
 
@@ -15,3 +15,29 @@ let a5 = function(num){return 5*num};
 let a6 = function(){return 50};
 
 log(a5(a6()));
+
+//Asynchronous JavaScript -> callbacks, setTimeout, Promises are regular AJAX.
+
+let b1 = 50;
+console.log(b1);
+setTimeout(function(){b1++; log(b1)},0);
+console.log(b1+10);
+
+let p = new Promise((resolve,reject) => {
+    resolve("Yo!")
+});
+log(p);
+
+
+/* OUTPUT */
+/* 
+
+5
+50
+250
+50
+60
+Promise { 'Yo!' }
+51
+
+*/
