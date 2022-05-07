3c747e076460cf6ada9856c2c12f852b4e139904
Som23Git <somesh.rokz@gmail.com>
2022-05-07 11:02:50 +0530

    Basic Demonstration of How To Create a Constructor & Classes for LinkedList in Data Structures with push() method and pop() method

--- data-structures/LinkedLists.js
+++ data-structures/LinkedLists.js
@@ -17,22 +17,24 @@ Node: {
     next: null
 }
 
-example of a proper linkedlist,
+example of a proper linkedlist object,
 
-Head -> head:{
+        {
+head -> head:{
             value: 7,
             next: {
                 value: 10,
                 next: {
                     value:23,
-Tail ->             tail: {
+tail ->             tail: {
                         value: 100,
-                        next: null
+tail.next               next: null
                     }
                 }
             }
 
         }
+    }
 
 If you take a look at the above linkedlist, the heading is pointing to the 
 first Node and the tail is pointing to next of the first last element.
@@ -81,8 +83,10 @@ If you see both the head and tail is pointing to the same Node i.e. value and ne
 
 */
 
-//In addition to the LinkedList Constructor, let us add the push() method for the LinkedList to add new Nodes to the list.
-//Using the same code again here,
+/* ------------------------------------------------------------------------------------------------------------------------ 
+In addition to the LinkedList Constructor, let us add the push() method for the LinkedList to add new Nodes to the list.
+Using the same code again here,
+------------------------------------------------------------------------------------------------------------------------ */
 
 
 //Step 1 Create a Node Class
@@ -131,3 +135,87 @@ length: 2
 tail: Node {value: 12, next: null}
 
 */
+
+/* ------------------------------------------------------------------------------------------------------------------------ 
+In addition to the LinkedList Constructor and push() method, let us add the pop() method to remove the Nodes from the last
+of the LinkedList
+
+Points to Understand in pop(), unlike push(), this had got 3 scenario or edge cases.
+
+1. Where the LinkedList got null elements i.e. there are NO elements in the List
+2. Where the LinkedList got only one element i.e. the both head and tail are pointing to that element.
+3. Where the LinkedList got multiple elements and we should remove the last in the list i.e. the head and tail are pointing 
+to different elements or NODES.
+
+Let's reuse the same code until push() method and append the pop() method code,
+------------------------------------------------------------------------------------------------------------------------ */
+
+//Step 1 Create a Node Class
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+//Step 2 Create a LinkedList Class with the Node Constructor
+class LinkedList{
+    constructor(value){
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    //Step 3 Add the push() method within the LinkedList Class so we can add more Nodes in the List
+    push(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop(){
+    //Scenario 1 where there is no values/elements in the List
+    if(!this.head) return undefined;     
+    
+    //Scenario 3 where there are multiple values/elements in the List
+    let temp = this.head;
+    let pre = this.head;
+
+    //This while loop, will increment the temp and pre variable until the temp.next === null, i.e. the last element
+    //Post that, the pre - will be assigned to the tail.
+    while(temp.next !== null){
+        pre = temp;
+        temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+
+    //Scenario 2 where there is only one value/element in the List
+    if(this.length === 0){
+        this.head = null;
+        this.tail = null;
+    }
+    
+    console.log(this);
+    return temp;
+
+}
+};
+
+//To test,
+
+let addLinkedList2 = new LinkedList(5);
+addLinkedList2.push(12);
+addLinkedList2.push(242);
+addLinkedList2.push(99);
+addLinkedList2.pop();
+
