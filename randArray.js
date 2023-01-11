b5be0c28f535b5f82e1a9dc6849a6017692c7a96
Som23Git <somesh.rokz@gmail.com>
2023-01-11 23:49:35 +0530

    Added a new program that outputs a random number from an array

---
+++ randArray.js
@@ -0,0 +1,25 @@
+const prompt=require("prompt-sync")({sigint:true}); 
+var array1 = [1,2,3,5,6,5];
+
+
+function randArray(){
+    // const array = new Array();
+    // const addArray = parseInt(prompt("Enter the Array: "));;
+    const randomIndex = Math.floor(Math.random() * array1.length);
+    console.log(`this is an array - ${array1}`);
+    console.log(`this is an random number - ${randomIndex}`);
+    return randomIndex;
+}
+
+
+let x = randArray();
+console.log(`This is the random array - ${array1[x]}`);
+
+//Output
+/* 
+
+this is an array - 1,2,3,5,6,5
+this is an random number - 2
+This is the random array - 3
+
+*/
